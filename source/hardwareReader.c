b4526a1735033d5e2a615b45d6c72f8f3a3df07f
Emil Gaustad <emiljga@stud.ntnu.no>
2026-02-19 14:05:35 +0100

    Updates to hardwareReader and elevator

--- source/hardwareReader.c
+++ source/hardwareReader.c
@@ -2,54 +2,49 @@
 #include "hardwareReader.h"
 #include <stdio.h>
 
-struct HardwareReader hardwareReader_constructor(){
+struct HardwareReader hardwareReader_constructor()
+{
     int floor = elevio_floorSensor();
-    struct Elevator elevator=elevator_constructor(floor);
-    struct HardwareReader hardwareReader={elevator, doReading};
+    struct Elevator elevator = elevator_constructor(floor);
+    struct HardwareReader hardwareReader = {&elevator, doReading};
     return hardwareReader;
 }
 
-void doReading(){
+void doReading(struct HardwareReader* reader)
+{
     int floor = elevio_floorSensor();
-    //int floor=1;
-    switch (floor){
-        case 1:
-            //update floor
-            break;
-        case 2:
-            //update floor
-            break;
-        case 3:
-            //update floor
-            break;
-        case 4:
-            //update floor
-            break;
-        default:
-            break;
+    switch (floor)
+    {
+    case -1:
+        break;
+    default:
+        reader->elevator->update_floor(reader->elevator, floor);
+        break;
     }
-    printf("\nFloor: %d\n",floor);
-    
-    int shouldStop=elevio_stopButton();
-    printf("shouldStop: %d\n",shouldStop);
-    //setStopped
-    
-    int obstructed=elevio_obstruction();
-    //set obstructed
-    
-    for(int floor=0;floor<N_FLOORS;floor++){
-        for(int button=0;button<N_BUTTONS;button++){
-            int isPressed=elevio_callButton(floor, button);
-            switch (isPressed){
-                case 1:
-                    //order
-                    printf("Make order: \n")
-                    break;
-                default:
-                    break;
+    printf("\nFloor: %d\n", floor); //debug
+
+    int shouldStop = elevio_stopButton(); //debug
+    printf("shouldStop: %d\n", shouldStop);
+    reader->elevator->set_stopped(reader->elevator,shouldStop);
+
+    int obstructed = elevio_obstruction();
+    reader->elevator->set_obstructed(reader->elevator, obstructed);
+
+    for (int floor = 0; floor < N_FLOORS; floor++)
+    {
+        for (int button = 0; button < N_BUTTONS; button++)
+        {
+            int isPressed = elevio_callButton(floor, button);
+            switch (isPressed)
+            {
+            case 1:
+                // order
+                printf("___________\nMake order (from hardwareReader):\nFloor: %d\nButton: %d", floor, button);
+                reader->elevator->order(reader->elevator, floor, (ButtonType)button);
+                break;
+            default:
+                break;
             }
         }
     }
-    
-    
 }
